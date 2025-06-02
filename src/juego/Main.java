ff3f97f971a251fbc2e2fddb84e8dd0733db743b
Manu Galán <manugalan0327@gmail.com>
2025-06-01 23:35:45 -0300

    new Sprites

--- src/juego/Main.java
+++ src/juego/Main.java
@@ -2,7 +2,7 @@ package juego;
 
 public class Main {
     public static void main(String[] args) {
-        //new ConfigPantalla(); // Lanza la interfaz de configuración
+        new ConfigPantalla(); // Lanza la interfaz de configuración
         new JuegoL();
     }
 }
