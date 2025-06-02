1caafe0ad957482ad9ac988d76d127c57ead8ddf
Manu Galán <manugalan0327@gmail.com>
2025-06-01 23:35:45 -0300

    new Sprites

--- src/juego/Lemming.java
+++ src/juego/Lemming.java
@@ -2,14 +2,22 @@ package juego;
 
 import java.awt.*;
 
-public class Lemming extends Container {
+public class Lemming extends Panel {
 
-    //Pixel limon = new Pixel(new Color(000));
+    private final String rutaImagen = 
 
     public Lemming(){
         //this.add(limon);
         //limon.setBounds(0,0,10,10);
     }
 
-    //public void actualizer(){limon.setBounds(0,limon.getY()-1,10,10);}
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        if (imagen != null) {
+            // Dibuja la imagen ocupando todo el tamaño del panel
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+
 }
