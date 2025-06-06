c1e2bcabc71d51a85fab82b1641120689bff628e
VelosoMauricio <mauriciojulian545@gmail.com>
2025-06-06 19:45:30 -0300

    Lanzador de los lemmings y configuracion de nivel y audio

--- src/lemmings/vista/PanelLemmings.java
+++ src/lemmings/vista/PanelLemmings.java
@@ -1,10 +1,8 @@
 package lemmings.vista;
 
 import lemmings.control.AudioPlayer;
-import lemmings.modelo.Lemming;
-import lemmings.modelo.Mapa;
-import lemmings.modelo.Nivel;
-import lemmings.modelo.BotonHabilidad;
+import lemmings.modelo.*;
+
 import java.util.List;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
@@ -31,10 +29,11 @@ public class PanelLemmings extends JPanel implements Runnable{
     private BotonHabilidad botonSeleccionado = null;
 
 
-    public PanelLemmings(JFrame ventana){
+    public PanelLemmings(JFrame ventana, int setNivel){
         setPreferredSize(new Dimension(800,600));
         setFocusable(true);
         setBackground(Color.black);
+        this.nivelNum = setNivel;
         //aca se cargan los niveles
         try {
             System.out.println("/lemmings/recursos/Nivel" + nivelNum + ".png");
@@ -50,7 +49,9 @@ public class PanelLemmings extends JPanel implements Runnable{
         Thread hilo = new Thread(this);
         hilo.start();
 
-        if (musicaFondo != null) {
+        if (musicaFondo != null && !Config.mute) {
+            musicaFondo.setVolume(Config.volumen);
+            System.out.println(Config.volumen);
             musicaFondo.loop(); // Empieza a reproducir la música en bucle
         }
 
