c6edd41d7af435e11f42c88737836d8368eb5ae7
VelosoMauricio <mauriciojulian545@gmail.com>
2025-06-08 18:24:29 -0300

    Ultima vercion del pong integrado

--- src/pong/Paleta.java
+++ src/pong/Paleta.java
@@ -2,6 +2,7 @@ package pong;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 public class Paleta {
     private int x, y;
@@ -9,15 +10,20 @@ public class Paleta {
     private int velocidad = 5;
     private boolean subiendo = false;
     private boolean bajando = false;
+    private boolean paletaDerecha;
 
     private int teclaArriba;
     private int teclaAbajo;
 
-    public Paleta(int x, int y, int teclaArriba, int teclaAbajo) {
+    public Paleta(int x, int y, int teclaArriba, int teclaAbajo, int multiplicador, boolean derecha) {
         this.x = x;
         this.y = y;
         this.teclaArriba = teclaArriba;
         this.teclaAbajo = teclaAbajo;
+        this.velocidad *= (multiplicador + 1)/2;
+        this.paletaDerecha = derecha;
+
+        Assets.init(); // carga las imagenes necesarias para las paletas
     }
 
     public void actualizar() {
@@ -29,8 +35,24 @@ public class Paleta {
     }
 
     public void dibujar(Graphics g) {
-        g.setColor(Color.WHITE);
-        g.fillRect(x, y, ancho, alto);
+        if ("original" == Config.skin){
+            g.setColor(Color.WHITE);
+            g.fillRect(x, y, ancho, alto);
+        }else if ("tropical" == Config.skin) {
+            Graphics2D g2d = (Graphics2D) g;
+
+            if (paletaDerecha)
+                g2d.drawImage(Assets.bananaDerecha, x, y, ancho + 20, alto, null);
+            else
+                g2d.drawImage(Assets.bananaIzquierda, x - 20, y, ancho + 20, alto, null);
+        }else if ("techno" == Config.skin){
+            Graphics2D g2d = (Graphics2D) g;
+
+            if (paletaDerecha)
+                g2d.drawImage(Assets.bladeDerecha, x - 10, y, ancho + 20, alto, null);
+            else
+                g2d.drawImage(Assets.bladeIzquierda, x -10, y, ancho + 20, alto, null);
+        }
     }
 
     public void teclaPresionada(KeyEvent e) {
@@ -54,4 +76,9 @@ public class Paleta {
     }
     public boolean getSubiendo(){return subiendo;}
     public boolean getBajando(){return bajando;}
+
+    // movimiento de cpu
+    public void setCpuSubiendo(boolean input){this.subiendo = input;};
+    public void setCpuBajando(boolean input){this.bajando = input;};
+    public int getY(){return this.y;}
 }
--- src/pong/Pelota.java
+++ src/pong/Pelota.java
@@ -71,20 +71,24 @@ public class Pelota {
         if (y <= 0 || y + Config.frameSuperior + tamaño >= 600) {
             velocidadY *= -1;
 
-            if (pared.isRunning())
-                pared.stop();
-            pared.setFramePosition(0); // reinicia el audio al comienzo
-            pared.start();
+            if(Config.sonidoActivado) {
+                if (pared.isRunning())
+                    pared.stop();
+                pared.setFramePosition(0); // reinicia el audio al comienzo
+                pared.start();
+            }
         }
 
         // Rebote con las paletas
         if (getRect().intersects(j1.getRect()) && choque.tangible){
             velocidadX *= -1;
 
-            if (paleta.isRunning())
-                paleta.stop();
-            paleta.setFramePosition(0); // reinicia el audio al comienzo
-            paleta.start();
+            if(Config.sonidoActivado) {
+                if (paleta.isRunning())
+                    paleta.stop();
+                paleta.setFramePosition(0); // reinicia el audio al comienzo
+                paleta.start();
+            }
 
             if (j1.getBajando() && velocidadY > -25){
                 velocidadY += entropia.nextInt(factor) + 1;
@@ -103,8 +107,10 @@ public class Pelota {
         if (getRect().intersects(j2.getRect()) && choque.tangible){
             velocidadX *= -1;
 
-            paleta.setFramePosition(0); // reinicia el audio al comienzo
-            paleta.start();
+            if(Config.sonidoActivado) {
+                paleta.setFramePosition(0); // reinicia el audio al comienzo
+                paleta.start();
+            }
 
             if (j2.getBajando() && velocidadY > -25){
                 velocidadY += entropia.nextInt(factor) + 1;
@@ -123,7 +129,13 @@ public class Pelota {
     }
 
     public void dibujar(Graphics g) {
-        g.setColor(Color.WHITE);
+        if (Config.skin == "original")
+            g.setColor(Color.WHITE);
+        if (Config.skin == "techno")
+            g.setColor(Color.RED);
+        if (Config.skin == "tropical")
+            g.setColor(Color.PINK);
+
         g.fillOval(x, y, tamaño, tamaño);
     }
 
@@ -146,6 +158,8 @@ public class Pelota {
     public Rectangle getRect() {
         return new Rectangle(x, y, tamaño, tamaño);
     }
+
+    public int getY(){return this.y;}
 }
 
 class Choque implements Runnable{
